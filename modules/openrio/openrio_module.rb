b21efae32ebe1c3ea358877d19c717e33c39a19a
Jaci R <jaci.brunning@gmail.com>
2017-09-27 18:02:28 +0800

    Finalize

--- modules/openrio/openrio_module.rb
+++ modules/openrio/openrio_module.rb
@@ -1,7 +1,8 @@
 require 'sinatra/base'
 
 class OpenRIOModule < Sinatra::Base
-    get "/gradlerio/recommended/" do
+    get "/gradlerio/recommended/?" do
+        content_type 'application/json'
         send_file "#{File.dirname(__FILE__)}/recommended_versions.json"
     end
 end
